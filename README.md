9792e1aaf340d9c390488a56030bb15f4434b245
zcfrank1st <zcfrank1st@foxmail.com>
2023-11-29 10:26:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+ComfyUI Yolov8
+=======
+
+> deadly simple yolov8 comfyui plugin
+
+
+
+## INSTANLL
+
+
+1. in comfyui `custom_nodes` dir and `https://github.com/zcfrank1st/Comfyui-Yolov8.git`
+2. put detect or seg models in comfyui `models/yolov8` dir
+
+## EXAMPLE
+
+![workflow_example](./workflow.png)
---
+++ comfy_yolov8.py
@@ -0,0 +1,106 @@
+import folder_paths
+from PIL import Image
+import numpy as np
+from ultralytics import YOLO
+import torch
+import os
+
+folder_paths.folder_names_and_paths["yolov8"] = ([os.path.join(folder_paths.models_dir, "yolov8")], folder_paths.supported_pt_extensions)
+
+class Yolov8DetectionNode:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "image": ("IMAGE",), 
+                "model_name": (folder_paths.get_filename_list("yolov8"), ),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    FUNCTION = "detect"
+    CATEGORY = "yolov8"
+
+    def detect(self, image, model_name):
+        # Convert tensor to numpy array and then to PIL Image
+        image_tensor = image
+        image_np = image_tensor.cpu().numpy()  # Change from CxHxW to HxWxC for Pillow
+        image = Image.fromarray((image_np.squeeze(0) * 255).astype(np.uint8))  # Convert float [0,1] tensor to uint8 image
+        
+        print(f'model_path: {os.path.join(folder_paths.models_dir, "yolov8")}/{model_name}')
+        model = YOLO(f'{os.path.join(folder_paths.models_dir, "yolov8")}/{model_name}')  # load a custom model
+        results = model(image)
+
+        # TODO load masks
+        # masks = results[0].masks
+
+        im_array = results[0].plot()  # plot a BGR numpy array of predictions
+        im = Image.fromarray(im_array[...,::-1])  # RGB PIL image
+
+        image_tensor_out = torch.tensor(np.array(im).astype(np.float32) / 255.0)  # Convert back to CxHxW
+        image_tensor_out = torch.unsqueeze(image_tensor_out, 0)
+
+        return (image_tensor_out,)
+
+class Yolov8SegNode:
+    def __init__(self) -> None:
+        ...
+    
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "image": ("IMAGE",), 
+                "model_name": (folder_paths.get_filename_list("yolov8"), ),
+                "class_id": ("INT", {"default": 0})
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE", "MASK",)
+    FUNCTION = "seg"
+    CATEGORY = "yolov8"
+
+    def seg(self, image, model_name, class_id):
+        # Convert tensor to numpy array and then to PIL Image
+        image_tensor = image
+        image_np = image_tensor.cpu().numpy()  # Change from CxHxW to HxWxC for Pillow
+        image = Image.fromarray((image_np.squeeze(0) * 255).astype(np.uint8))  # Convert float [0,1] tensor to uint8 image
+        
+        print(f'model_path: {os.path.join(folder_paths.models_dir, "yolov8")}/{model_name}')
+        model = YOLO(f'{os.path.join(folder_paths.models_dir, "yolov8")}/{model_name}')  # load a custom model
+        results = model(image)
+
+
+        # get array results
+        masks = results[0].masks.data
+        boxes = results[0].boxes.data
+        # extract classes
+        clss = boxes[:, 5]
+        # get indices of results where class is 0 (people in COCO)
+        people_indices = torch.where(clss == class_id)
+        # use these indices to extract the relevant masks
+        people_masks = masks[people_indices]
+        # scale for visualizing results
+        people_mask = torch.any(people_masks, dim=0).int() * 255
+
+        im_array = results[0].plot()  # plot a BGR numpy array of predictions
+        im = Image.fromarray(im_array[...,::-1])  # RGB PIL image
+
+        image_tensor_out = torch.tensor(np.array(im).astype(np.float32) / 255.0)  # Convert back to CxHxW
+        image_tensor_out = torch.unsqueeze(image_tensor_out, 0)
+
+        return (image_tensor_out, people_mask)
+
+NODE_CLASS_MAPPINGS = {
+    "Yolov8Detection": Yolov8DetectionNode,
+    "Yolov8Segmentation": Yolov8SegNode,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Yolov8Detection": "detection",
+    "Yolov8Segmentation": "seg",
+}
+
